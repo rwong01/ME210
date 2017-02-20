a9f6e014dcfc9f7454491b9379c020e5942574f0
Davy Ragland <dragland@stanford.edu>
2017-02-19 23:31:49 -0800

    Updated main overview.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,22 @@
 #include "Arduino.h"
 
 /***********************************  BOOT  ***********************************/
+static const uint32_t RUNTIME_CONST = 1000 * 60 * 10;
+
 int main(void) {
   Serial.begin(9600);
 /***********************************  MAIN  ***********************************/
-  while(true) {
+  while(millis() < RUNTIME_CONST) {
     Serial.println("RISKY BISCUITS");
-    delay(10);
+    //exit start zone
+    //trip all bariiers
+    //goto 1st t
+    //launch 3 balls
+    //goto 2nd t
+    //launch 3 balls
+    //goto 3rd t
+    //launch 3 balls
+    //goto start
+    //wait 10 seconds to load
   }
 }
