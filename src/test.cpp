c64ce87771690dc2c87524ab615b213ad55f9cb1
Davy Ragland <dragland@stanford.edu>
2017-03-10 10:07:30 -0800

    Tuned constants.

--- src/test.cpp
+++ src/test.cpp
@@ -13,28 +13,31 @@
 #include "Config.h"
 #include <AccelStepper.h>
 
-/***********************************  BOOT  ***********************************/
-AccelStepper stepper(1, MOTOR_STEP_STEP, MOTOR_STEP_DIR);
-int main() {
-  uint16_t LOADER_SPEED = 350;
-  Serial.begin(9600);
-  pinMode(MOTOR_FIRE_FWD,   OUTPUT);
-  pinMode(MOTOR_STEP_DIR,   OUTPUT);
-  pinMode(MOTOR_STEP_STEP,  OUTPUT);
-  digitalWrite(MOTOR_FIRE_FWD, HIGH);
-  stepper.setMaxSpeed(LOADER_SPEED);
-  stepper.setSpeed(LOADER_SPEED);
-  uint32_t start = millis();
-  uint32_t startt;
-
-/***********************************  MAIN  ***********************************/
-  Serial.print("warming up...");
-  while ((millis() - start) <= 5000);
-  startt = millis();
-  while(true) {
-    if ((millis() - startt) <= LAUNCH_TIMEOUT) {
-      stepper.runSpeed();
-    }
-    delay(BUFFER_CLEAR_TIME_STEP);
-  }
-}
+// /***********************************  BOOT  ***********************************/
+// AccelStepper stepper(1, MOTOR_STEP_STEP, MOTOR_STEP_DIR);
+// int main() {
+//   uint16_t LOADER_SPEED = 280;
+//   Serial.begin(9600);
+//   pinMode(MOTOR_FIRE_FWD,   OUTPUT);
+//   pinMode(MOTOR_STEP_DIR,   OUTPUT);
+//   pinMode(MOTOR_STEP_STEP,  OUTPUT);
+//   digitalWrite(MOTOR_FIRE_FWD, HIGH);
+//   stepper.setMaxSpeed(LOADER_SPEED);
+//   stepper.setSpeed(LOADER_SPEED);
+//   uint32_t start = millis();
+//   uint32_t startt;
+//
+// /***********************************  MAIN  ***********************************/
+//   Serial.print("warming up...");
+//   while ((millis() - start) <= 5000);
+//   startt = millis();
+//   for(int i = 0; i < 13; i++) {
+//     while ((millis() - startt) <= LAUNCH_TIMEOUT) {
+//       stepper.runSpeed();
+//       delay(BUFFER_CLEAR_TIME_STEP);
+//     }
+//     stepper.setSpeed(LOADER_SPEED);
+//     delay(LOAD_TIMEOUT);
+//     startt = millis();
+//   }
+// }
